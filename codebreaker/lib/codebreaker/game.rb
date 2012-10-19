76c1580b59b86301154d732e00c06cb61228ff79
James Bowkett <james_bowkett@lyagushka.co.uk>
2012-10-19 12:17:03 +0100

    refactorted to current inject as a temporary variable

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -18,23 +18,15 @@ module Codebreaker
     end
 
     def exact_match_count(guess)
-      exact_match_count = 0
-      (0..3).map do |index|
-        if exact_match?(guess, index)
-          exact_match_count += 1
-        end
+      (0..3).inject(0) do |current_count, index|
+        current_count += (exact_match?(guess, index) ? 1 :0)
       end
-      exact_match_count
     end
 
     def number_match_count(guess)
-      number_match_count = 0
-      (0..3).map do |index|
-        if number_match?(guess, index)
-          number_match_count += 1
-        end
+      (0..3).inject(0) do |current_count, index|
+        current_count += (number_match?(guess, index) ? 1 :0)
       end
-      number_match_count
     end
 
 
